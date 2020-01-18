e5080a3b7aa690c53e50ae0a5234ac57b7a0dd17
Josh Peak <neozenith.dev@gmail.com>
2020-01-19 10:32:37 +1100

    pp298, load dataset

--- ch10.py
+++ ch10.py
@@ -1,5 +1,21 @@
+import logging
+
 import tensorflow as tf
 from tensorflow import keras
 
-print(tf.__version__)
-print(keras.__version__)
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+log.info(tf.__version__)
+log.info(keras.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+(X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
+
+log.info(f"{X_train_full.shape}, {y_train_full.shape}")
+
+X_valid, X_train = X_train_full[:5000] / 255.0, X_train_full[5000:] / 255.0
+y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+
+class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle Boot"]
+
+log.info(class_names[y_train[0]])
